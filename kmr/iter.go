867672f61adab76f20494a4be5c04266ae61a44a
Amit Lavon <amit.lavon@weizmann.ac.il>
2022-10-02 16:28:08 +0300

    Add new smpkmers

---
+++ kmr/iter.go
@@ -0,0 +1,47 @@
+package kmr
+
+import (
+	"fmt"
+	"io"
+	"path/filepath"
+
+	"github.com/fluhus/kwas/aio"
+)
+
+// IterTuplesFile iterates the given file, calling forEach on each tuple.
+// Reuses init as input to forEach, so keeping instances should use Copy().
+func IterTuplesFile[T Tuple](file string, init T, forEach func(T) error) error {
+	f, err := aio.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	for err = init.Decode(f); err == nil; err = init.Decode(f) {
+		if err := forEach(init); err != nil {
+			return err
+		}
+	}
+	if err != io.EOF {
+		return err
+	}
+	return nil
+}
+
+// IterTuplesFiles iterates the files matching the given glob pattern,
+// calling forEach on each tuple.
+// Reuses init as input to forEach, so keeping instances should use Copy().
+func IterTuplesFiles[T Tuple](glob string, init T, forEach func(T) error) error {
+	files, err := filepath.Glob(glob)
+	if err != nil {
+		return err
+	}
+	if len(files) == 0 {
+		return fmt.Errorf("found 0 files")
+	}
+	for _, file := range files {
+		if err := IterTuplesFile(file, init, forEach); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ smpkmers/smpkmers3.go
@@ -0,0 +1,109 @@
+// Randomly samples kmers and sample IDs from HAS files.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+
+	"github.com/fluhus/kwas/aio"
+	"github.com/fluhus/kwas/kmr"
+	"github.com/fluhus/kwas/progress"
+	"github.com/fluhus/kwas/util"
+)
+
+var (
+	fin  = flag.String("i", "", "Input HAS file glob template")
+	fout = flag.String("o", "", "Output file")
+	n    = flag.Int("n", 0, "Subsample exactly n kmers")
+	r    = flag.Float64("r", 0, "Subsample kmers with probability 1/r")
+	s    = flag.Uint("s", 0, "Subsample sample IDs with probability 1/s")
+)
+
+func main() {
+	flag.Parse()
+	if sumBools(*n != 0, *r != 0, *s != 0) != 1 {
+		util.Die(fmt.Errorf("please use either -n, -r or -s"))
+	}
+	if *r != 0 {
+		ratio := 1.0 / *r
+		kept := 0
+		out, err := aio.Create(*fout)
+		util.Die(err)
+		pt := progress.NewTimerFunc(func(i int) string {
+			return fmt.Sprintf("%d (kept %d)", i, kept)
+		})
+		util.Die(
+			kmr.IterTuplesFiles(*fin, &kmr.HasTuple{}, func(t *kmr.HasTuple) error {
+				pt.Inc()
+				if rand.Float64() < ratio {
+					kept++
+					return t.Encode(out)
+				}
+				return nil
+			}))
+		util.Die(out.Close())
+		pt.Done()
+	} else if *n != 0 {
+		r := util.NewReservoir[*kmr.HasTuple](*n)
+		pt := progress.NewTimer()
+		util.Die(
+			kmr.IterTuplesFiles(*fin, &kmr.HasTuple{}, func(t *kmr.HasTuple) error {
+				r.Add(t.Copy().(*kmr.HasTuple))
+				pt.Inc()
+				return nil
+			}))
+		pt.Done()
+		out, err := aio.Create(*fout)
+		util.Die(err)
+		for _, t := range r.Sample {
+			util.Die(t.Encode(out))
+		}
+		util.Die(out.Close())
+	} else if *s != 0 {
+		s := uint64(*s)
+		out, err := aio.Create(*fout)
+		util.Die(err)
+		pt := progress.NewTimer()
+		util.Die(
+			kmr.IterTuplesFiles(*fin, &kmr.HasTuple{}, func(t *kmr.HasTuple) error {
+				var samples []int
+				for _, i := range t.Samples {
+					if hashInt(i)%s == 0 {
+						samples = append(samples, i)
+					}
+				}
+				t.Samples = samples
+				pt.Inc()
+				return t.Encode(out)
+			}))
+		util.Die(out.Close())
+		pt.Done()
+	}
+	fmt.Println("Done")
+}
+
+// Buffer for int hashing.
+var hashBuf = make([]byte, 8)
+
+// Returns the hash value of the given int.
+func hashInt(n int) uint64 {
+	if n < 0 {
+		panic(fmt.Sprintf("invalid i: %d", n))
+	}
+	for i := range hashBuf {
+		hashBuf[i] = byte((n >> (i * 8)) & 255)
+	}
+	return util.Hash64(hashBuf)
+}
+
+// Returns the number of true's.
+func sumBools(b ...bool) int {
+	sum := 0
+	for _, bb := range b {
+		if bb {
+			sum++
+		}
+	}
+	return sum
+}
