cf492fb978e2eef4ee4a2e4ec5626aa0e2725859
Amit Lavon <amit.lavon@weizmann.ac.il>
2024-03-11 12:51:44 +0200

    Update projectpopstr

--- projectpopstr/projectpopstr.go
+++ projectpopstr/projectpopstr.go
@@ -7,16 +7,23 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/fluhus/biostuff/sequtil"
 	"github.com/fluhus/gostuff/aio"
+	"github.com/fluhus/gostuff/gnum"
 	"github.com/fluhus/gostuff/jio"
-	"github.com/fluhus/kwas/kmc"
-	"github.com/fluhus/kwas/kmr"
+	"github.com/fluhus/gostuff/ppln/v2"
+	"github.com/fluhus/kwas/kmr/v2"
 	"github.com/fluhus/kwas/progress"
 	"github.com/fluhus/kwas/util"
 )
 
+const (
+	threads = 4
+	usePPLN = false
+)
+
 var (
-	fin   = flag.String("i", "", "Input sample fastq file")
+	fin   = flag.String("i", "", "Input sample dump file")
 	fcomp = flag.String("c", "", "Input components JSON file")
 	fout  = flag.String("o", "", "Output JSON file")
 )
@@ -28,16 +35,55 @@ func main() {
 	comps, err := loadComponents(*fcomp)
 	util.Die(err)
 	fmt.Println("Found", len(comps), "components")
-	vals := make([]float64, len(comps))
+	var vals []float64
+	var bufs [][]byte
+	if usePPLN {
+		vals = make([]float64, len(comps)*threads)
+		bufs = make([][]byte, threads)
+	} else {
+		vals = make([]float64, len(comps))
+	}
 
-	fmt.Println("Reading fastq")
+	fmt.Println("Reading kmers")
 	pt := progress.NewTimer()
-	kmc.KMC2(func(kmer []byte, count int) {
-		for i := range vals {
-			vals[i] += comps[i][string(kmer)]
+	if usePPLN {
+		err = ppln.NonSerial[kmr.Kmer, struct{}](threads,
+			kmr.IterKmersFile(*fin),
+			func(kmer kmr.Kmer, g int) (struct{}, error) {
+				if g < 0 || g >= threads {
+					panic(fmt.Sprintf("bad g: %v", g))
+				}
+				bufs[g] = sequtil.DNAFrom2Bit(bufs[g][:0], kmer[:])
+				for i, c := range comps {
+					vals[i*threads+g] += c[string(bufs[g])]
+				}
+				return struct{}{}, nil
+			}, func(a struct{}) error {
+				pt.Inc()
+				return nil
+			})
+		util.Die(err)
+	} else {
+		var buf []byte
+		for kmer, err := range kmr.IterKmersFile(*fin) {
+			util.Die(err)
+			buf = sequtil.DNAFrom2Bit(buf[:0], kmer[:])[:kmr.K]
+			for i := range vals {
+				vals[i] += comps[i][string(buf)]
+			}
+			pt.Inc()
 		}
-	}, *fin, kmc.OptionK(kmr.K), kmc.OptionThreads(2))
+	}
 	pt.Done()
+	util.Die(err)
+
+	if usePPLN {
+		vals2 := make([]float64, len(comps))
+		for i := range vals2 {
+			vals2[i] = gnum.Sum(vals[i*threads : (i+1)*threads])
+		}
+		vals = vals2
+	}
 
 	fmt.Println("Writing projection")
 	util.Die(jio.Save(*fout, vals))
